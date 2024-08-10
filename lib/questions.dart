710ebf46e81520189bb71b6dcfaa66cf8f0d47b1
Naveen S <naveen121101@gmail.com>
2024-08-10 21:29:12 +0530

    First Commit

---
+++ lib/questions.dart
@@ -0,0 +1,6 @@
+//ignore_for_file:prefer_typing_uninitialized_variables
+class Question {
+  String questionText = "";
+  bool questionAnswer = true;
+  Question({required this.questionText, required this.questionAnswer});
+}
